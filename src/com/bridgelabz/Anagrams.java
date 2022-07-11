db304d3c32681ae057329edbe6163e5b191b683d
sagarchavan07 <chavansagar96044@gmail.com>
2022-07-11 12:06:27 +0530

    added anagram

--- src/com/bridgelabz/Anagrams.java
+++ src/com/bridgelabz/Anagrams.java
@@ -1,18 +1,33 @@
 package com.bridgelabz;
 
+import javax.swing.*;
+
 public class Anagrams {
     public static void main(String[] args) {
-        boolean anagram=false;
+        boolean anagram=true;
 
-        String str1="abcd";
-        String str2="bcad";
+        String str1="abcdz";
+        String str2="bczad";
 
-        if (str2.length() == str2.length()) {
-            for ()
-
+        if (str2.length() == str2.length())
+        {
+            for (int i = 0; i < str1.length(); i++) {
+                int checkcount=0;
+                for (int j = 0; j < str2.length(); j++) {
+                    if (str1.charAt(i) == str2.charAt(j)) {
+                        checkcount++;
+                    }
+                }
+                if (checkcount == 0) {
+                    anagram=false;
+                }
+            }
         }else {
-            anagram=false
+            anagram=false;
         }
 
+        System.out.println("Anagram : "+anagram);
+
     }
+
 }
